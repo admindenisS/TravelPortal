b1df6d592e61b16fcb95502f949d0faec167af9d
Denis <173691105+admindenisS@users.noreply.github.com>
2024-12-20 23:25:59 +0300

    work №1

---
+++ public/scripts/menu.js
@@ -0,0 +1,29 @@
+function openMenu(device) {
+    let menuElement = null;
+
+    console.log(device);
+    
+    if (device == "mobile") {
+        menuElement = document.getElementById('menu-mob');
+    } else if (device == "pc") {
+        menuElement = document.getElementById('menu-pc');
+    }
+
+    if (menuElement === null) return;
+    let status = menuElement.getAttribute("data-active");
+    
+    console.log(status);
+    console.log(menuElement);
+    
+    if (status == 1) {
+        document.body.style.overflowY = "unset";
+        menuElement.style.display = "none";
+        menuElement.setAttribute("data-active", 0);
+    } else {
+        document.body.style.overflowY = "hidden";
+        menuElement.removeAttribute("hidden");
+        menuElement.style.display = "flex";
+        menuElement.setAttribute("data-active", 1);
+    }
+    console.log(menuElement);
+}
