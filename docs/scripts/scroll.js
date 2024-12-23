f036079bf93c4ce42598044f7845ca956baa4c59
admindenisS <173691105+admindenisS@users.noreply.github.com>
2024-12-23 17:22:24 +0300

    about ready

---
+++ docs/scripts/scroll.js
@@ -0,0 +1,48 @@
+let slider = document.getElementById("images");
+let progress = document.getElementById("progress");
+let progressContainer = document.getElementsByClassName("progress-container").item(0);
+
+if (slider && progress) {
+    let progressLength = progressContainer.clientWidth - progress.clientWidth;
+
+    slider.addEventListener('scroll', function () {
+
+        let move = (slider.scrollLeft / (slider.scrollWidth - slider.clientWidth)) * progressLength;
+        move = Math.max(0, Math.min(move, progressLength));
+
+        progress.style.left = `${Math.abs(move)}px`;
+    });
+}
+
+if (window.matchMedia("(pointer: fine)").matches) {
+    const container = document.querySelector('#images');
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    container.addEventListener('mousedown', (e) => {
+        isDown = true;
+        container.classList.add('active');
+        startX = e.pageX - container.offsetLeft;
+        scrollLeft = container.scrollLeft;
+    });
+
+    container.addEventListener('mouseleave', () => {
+        isDown = false;
+        container.classList.remove('active');
+    });
+
+    container.addEventListener('mouseup', () => {
+        isDown = false;
+        container.classList.remove('active');
+    });
+
+    container.addEventListener('mousemove', (e) => {
+        if (!isDown) return; // если мышь не нажата, выходим
+        e.preventDefault();
+        const x = e.pageX - container.offsetLeft;
+        const walk = (x - startX) * 2; // скорость прокрутки
+        container.scrollLeft = scrollLeft - walk;
+    });
+}
