13edd7c2c424f52493ab939557d09c602a972d54
Denis <173691105+admindenisS@users.noreply.github.com>
2024-12-31 01:59:50 +0300

    index loading optimization

--- docs/scripts/imagewrapper.js
+++ docs/scripts/imagewrapper.js
@@ -1,7 +1,21 @@
+let webpSupport = false;
+
+(() => {
+	var img = new Image();
+	img.onload = function () {
+		var result = img.width > 0 && img.height > 0;
+		webpSupport = true;
+	};
+	img.onerror = function () {
+		webpSupport = false;
+	};
+	img.src = "data:image/webp;base64,UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA";
+})();
+
 let currentAnimation = null;
 
-function changeImage(newUrl, image) {
-	let currentUrl = image.src.trim().replace(/.*assets\/|[)]+/g, "");
+function changeImage(newUrl, target, source) {
+	let currentUrl = target.src.trim().replace(/.*assets\/|[)]+/g, "");
 	if (currentUrl === newUrl) return;
 
 	if (currentAnimation) {
@@ -11,13 +25,16 @@ function changeImage(newUrl, image) {
 	let bannerText = document.getElementsByClassName("banner-text")[0];
 	if (bannerText) bannerText.style.display = "none";
 
-	let currentInimation = (image.animate([{ opacity: 1 }, { opacity: 0 }, { opacity: 1, content: `url(./assets/${newUrl})` }], {
-		duration: 300,
-		easing: "ease-in-out",
-		fill: "forwards",
-	}).onfinish = () => {
-		image.src = `./assets/${newUrl}`;
-		if (newUrl === "banner.png") {
+	let currentInimation = (target.animate(
+		[{ opacity: 1 }, { opacity: 0 }, { opacity: 1, content: `url(./assets/${newUrl}.${webpSupport ? "webp" : "png"})` }],
+		{
+			duration: 300,
+			easing: "ease-in-out",
+			fill: "forwards",
+		}
+	).onfinish = () => {
+		target.src = `/assets/${newUrl}.${webpSupport ? "webp" : "png"}`;
+		if (newUrl === source) {
 			bannerText.style.display = "flex";
 		}
 		currentAnimation = null;
@@ -27,7 +44,8 @@ function changeImage(newUrl, image) {
 let isMouseOverSubimage = false;
 
 document.querySelectorAll("[data-subimage]").forEach(function (element) {
-	let banner = document.getElementById("banner-image");
+	if (!element.dataset.target) return;
+	let targetElement = element.dataset.target == "this" ? element : document.querySelector(element.dataset.target);
 
 	element.addEventListener("mouseenter", function (event) {
 		isMouseOverSubimage = true;
@@ -35,14 +53,14 @@ document.querySelectorAll("[data-subimage]").forEach(function (element) {
 
 		if (!target || !target.dataset.subimage) return;
 
-		changeImage(target.dataset.subimage, banner);
+		changeImage(target.dataset.subimage, targetElement);
 	});
 
 	element.addEventListener("mouseleave", function (event) {
 		isMouseOverSubimage = false;
 		setTimeout(() => {
 			if (!isMouseOverSubimage) {
-				changeImage("banner.png", banner);
+				changeImage(element.dataset.source, targetElement, element.dataset.source);
 			}
 		}, 150);
 	});
