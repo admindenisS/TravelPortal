c50b7f7ee4b55b2aaff3fbc84519a2cf635a9401
Denis <173691105+admindenisS@users.noreply.github.com>
2025-01-06 18:47:06 +0300

    optimization and some bug fixes

--- docs/scripts/imagewrapper.js
+++ docs/scripts/imagewrapper.js
@@ -12,43 +12,65 @@ let webpSupport = false;
 	img.src = "data:image/webp;base64,UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA";
 })();
 
-let currentAnimation = null;
+let currentAnimation = [];
 
 function changeImage(newUrl, target, source) {
 	let currentUrl = target.src.trim().replace(/.*assets\/|[)]+/g, "");
 	if (currentUrl === newUrl) return;
 
-	if (currentAnimation) {
-		currentAnimation.cancel();
+	if (currentAnimation[newUrl]) {
+		currentAnimation[newUrl].cancel();
 	}
 
 	let bannerText = document.getElementsByClassName("banner-text")[0];
 	if (bannerText) bannerText.style.display = "none";
 
-	let currentInimation = (target.animate(
-		[{ opacity: 1 }, { opacity: 0 }, { opacity: 1, content: `url(./assets/${newUrl}.${webpSupport ? "webp" : "png"})` }],
+	currentAnimation[newUrl] = target.animate(
+		[{ opacity: 1 }, { opacity: 0 }, { opacity: 1, content: `url(/assets/${newUrl}.${webpSupport ? "webp" : "png"})` }],
 		{
 			duration: 300,
 			easing: "ease-in-out",
 			fill: "forwards",
 		}
-	).onfinish = () => {
+	);
+
+	currentAnimation[newUrl].onfinish = () => {
 		target.src = `/assets/${newUrl}.${webpSupport ? "webp" : "png"}`;
-		if (newUrl === source) {
+		if (bannerText && newUrl === source) {
 			bannerText.style.display = "flex";
 		}
-		currentAnimation = null;
-	});
+		currentAnimation[newUrl] = null;
+	};
 }
 
-let isMouseOverSubimage = false;
+function preloadImage(url) {
+	if (!url) {
+		console.error("URL is required to preload an image.");
+		return;
+	}
+
+	let img = new Image();
+	img.src = `/assets/${url}.${webpSupport ? "webp" : "png"}`;
+
+	img.onload = () => {
+		console.log(`Image preloaded: ${img.src}`);
+	};
+
+	img.onerror = () => {
+		console.error(`Failed to preload image: ${img.src}`);
+	};
+}
+
+let isMouseOverSubimage = {};
 
 document.querySelectorAll("[data-subimage]").forEach(function (element) {
 	if (!element.dataset.target) return;
 	let targetElement = element.dataset.target == "this" ? element : document.querySelector(element.dataset.target);
 
+	if (element.dataset.subimage) preloadImage(element.dataset.subimage);
+
 	element.addEventListener("mouseenter", function (event) {
-		isMouseOverSubimage = true;
+		isMouseOverSubimage[targetElement.dataset.source] = true;
 		let target = event.target;
 
 		if (!target || !target.dataset.subimage) return;
@@ -57,9 +79,9 @@ document.querySelectorAll("[data-subimage]").forEach(function (element) {
 	});
 
 	element.addEventListener("mouseleave", function (event) {
-		isMouseOverSubimage = false;
+		isMouseOverSubimage[targetElement.dataset.source] = false;
 		setTimeout(() => {
-			if (!isMouseOverSubimage) {
+			if (!isMouseOverSubimage[targetElement.dataset.source]) {
 				changeImage(element.dataset.source, targetElement, element.dataset.source);
 			}
 		}, 150);
