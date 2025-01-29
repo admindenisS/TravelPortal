6502565bfafc6bacc0fc8b746a38309d2cd7387d
admindenisS <173691105+admindenisS@users.noreply.github.com>
2025-01-29 17:29:12 +0300

    fixes

--- docs/pages/afisha.php
+++ docs/pages/afisha.php
@@ -1,17 +1,17 @@
 <?php
-require_once '../../vendor/autoload.php';
-require_once '/scripts/database.php';
+require_once __DIR__ . '/../app/vendor/autoload.php';
+require_once __DIR__ . '/../scripts/database.php';
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-$loader = new FilesystemLoader('./templates');
+$loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader);
 
 $limit = 5;
 
 if (isset($_GET['page']) && ctype_digit($_GET['page'])) {
-    $page = (int)$_GET['page'];
+    $page = (int) $_GET['page'];
     $offset = ($page - 1) * $limit;
 } else {
     $offset = 0;
@@ -26,9 +26,18 @@ if (isset($_GET['sort']) && $_GET['sort'] == 'old') {
 
 $db = Database::getInstance()->getConnection();
 
-$result = $db->execute_query("SELECT COUNT(*) as `total` FROM `afisha`");
-$rows = $result->fetch_assoc();
-$totalPages = round((int)$rows['total'] / $limit, 0, PHP_ROUND_HALF_DOWN);
+try {
+    $TotalResult = $db->query("SELECT COUNT(*) as `total` FROM `afisha`");
+
+    if (!$TotalResult) {
+        throw new Exception("Ошибка выполнения запроса: " . $db->error);
+    }
+
+    $TotalRows = $TotalResult->fetch_assoc();
+    $totalPages = round((int) $TotalRows['total'] / $limit, 0, PHP_ROUND_HALF_DOWN);
+} catch (Exception $e) {
+    echo "Произошла ошибка: " . $e->getMessage();
+}
 
 $stmt = $db->prepare("SELECT * FROM `afisha` ORDER BY `date` $order LIMIT ? OFFSET ?");
 $stmt->bind_param('ii', $limit, $offset);
--- docs/scripts/limiter.js
+++ docs/scripts/limiter.js
@@ -12,12 +12,12 @@ function validate() {
     if (element.value.length > 4 && element.value.length < 0) return false;
 
     let form = document.getElementsByTagName("form")[0];
-    fetch("https://ladoganew.ru/" + element.value, {
+    fetch("https://ladoganew.ru/selfie/" + element.value, {
         method: 'GET',
         'Content-Type': 'application/json'
     }).then((res) => {
         if (res.status == 200) {
-            location.href = "https://ladoganew.ru/" + element.value;
+            location.href = "https://ladoganew.ru/selfie/" + element.value;
         }
         console.log(res);
 
