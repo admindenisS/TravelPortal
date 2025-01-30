eaff7449a256534c00fcc55f80a80575b684f10e
Denis <173691105+admindenisS@users.noreply.github.com>
2025-01-30 23:53:05 +0300

    turn off pip in video

--- docs/scripts/drone.js
+++ docs/scripts/drone.js
@@ -14,6 +14,7 @@ window.onload = () => {
     videoElement.muted = true;
     videoElement.setAttribute("muted", "");
     videoElement.setAttribute("playsinline", "");
+    videoElement.setAttribute("pip", "false")
     videoElement.id = "drone";
 
     let sourceWebm = document.createElement("source");
